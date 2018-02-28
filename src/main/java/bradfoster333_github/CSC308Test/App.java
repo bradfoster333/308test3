76cc8481808aabbafb4435b369992ce8665ec19c
bradfoster333 <bradfoster333@gmail.com>
2018-02-28 10:41:06 -0800

    Add files via upload

---
+++ src/main/java/bradfoster333_github/CSC308Test/App.java
@@ -0,0 +1,13 @@
+package bradfoster333_github.CSC308Test;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+	public static void main(String[] args)
+	{
+		System.out.println("PLEASE GIVE ME 100% ON THIS TERM");
+	}
+}
